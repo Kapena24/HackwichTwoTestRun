141f3b7e91bdd0a862febc1fb4ba0c4ed15ad586
Kapena Kaaihue <142773521+Kapena24@users.noreply.github.com>
2023-09-07 13:56:58 -1000

    Part 1-6 Thursday class

---
+++ HackwichTwo 9.5.25/secondViewController.swift
@@ -0,0 +1,26 @@
+//
+//  secondViewController.swift
+//  HackwichTwo 9.5.25
+//
+//  Created by Kapena Kaaihue on 9/7/23.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func changeColorButtonPressed(_ sender: Any) {
+        self.view.backgroundColor = UIColor.blue
+    }
+
+
+    
+   
+
+}

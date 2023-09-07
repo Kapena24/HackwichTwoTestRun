6fa089e6851733af4e693a2e965916a3e98df4c0
Kapena Kaaihue <142773521+Kapena24@users.noreply.github.com>
2023-09-07 13:02:46 -1000

    Did in class Part 1-7

--- HackwichTwo 9.5.25/ViewController.swift
+++ HackwichTwo 9.5.25/ViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+// part 8: Declare and assign three string variables
 
+var partOne = "welcome, "
+var PartTwo = " You're entering my app. "
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
